b06f9672cc8de172203dd976dbaeb4699a871600
John <johnmsaylor@gmail.com>
2020-06-29 16:37:39 -0400

    buttons challenge

---
+++ buttons-container/js/buttonsGrid.js
@@ -0,0 +1,27 @@
+const btns = document.getElementById("btns");
+const arr = [1, 2, 3, 4, 6, 7, 8, 9];
+
+// for (let x = 1; x <= 9; x++) {
+//   const id = "btn" + x.toString();
+//   const btn = createButton(id, arr[x - 1]);
+//   btns.appendChild(btn);
+// }
+
+// function createButton(id, innerHTML) {
+//   const btn = document.createElement("div");
+//   btn.className = "button";
+//   btn.id = id;
+//   btn.onclick = shiftNumbers;
+//   btn.innerHTML = innerHTML;
+//   return btn;
+// }
+
+function shiftNumbers() {
+  //   const all = document.getElementById("btns").children;
+  //   console.log(all.btn1);
+  //   for (elem in all) {
+  //     console.log(elem);
+  //   }
+  //   btn.innerHTML = arr[parseInt(btn.innerHTML) + 1];
+  console.log("ey");
+}
