a9df6f8ca246fb7f751195bf993c26b55fef7a29
John <johnmsaylor@gmail.com>
2020-07-22 20:04:34 -0400

    shifty buttons exercise

--- buttons-container/js/buttonsGrid.js
+++ buttons-container/js/buttonsGrid.js
@@ -1,27 +1,28 @@
-const btns = document.getElementById("btns");
-const arr = [1, 2, 3, 4, 6, 7, 8, 9];
-
-// for (let x = 1; x <= 9; x++) {
-//   const id = "btn" + x.toString();
-//   const btn = createButton(id, arr[x - 1]);
-//   btns.appendChild(btn);
-// }
-
-// function createButton(id, innerHTML) {
-//   const btn = document.createElement("div");
-//   btn.className = "button";
-//   btn.id = id;
-//   btn.onclick = shiftNumbers;
-//   btn.innerHTML = innerHTML;
-//   return btn;
-// }
-
 function shiftNumbers() {
-  //   const all = document.getElementById("btns").children;
-  //   console.log(all.btn1);
-  //   for (elem in all) {
-  //     console.log(elem);
-  //   }
-  //   btn.innerHTML = arr[parseInt(btn.innerHTML) + 1];
-  console.log("ey");
+  //skips 5
+  let buttons = [
+    "btn1",
+    "btn2",
+    "btn3",
+    "btn6",
+    "btn9",
+    "btn8",
+    "btn7",
+    "btn4",
+  ];
+
+  let numbers = [];
+
+  buttons.forEach((id) => {
+    const button = document.getElementById(id);
+    numbers.push(button.innerHTML);
+  });
+
+  let splice = numbers.splice(-1);
+  numbers.unshift(splice[0]);
+
+  for (let x = 0; x < buttons.length; x++) {
+    const button = document.getElementById(buttons[x]);
+    button.innerHTML = numbers[x];
+  }
 }
