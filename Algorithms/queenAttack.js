b3933aeacb51d1dc74cea5d36d28867820563f35
John <johnmsaylor@gmail.com>
2020-08-23 21:16:37 -0400

    too slow, not passing all test cases

--- Algorithms/queenAttack.js
+++ Algorithms/queenAttack.js
@@ -1,6 +1,17 @@
 // Complete the queensAttack function below.
 function queensAttack(n, k, r_q, c_q, obstacles) {
+  let counter = 0;
   const board = [];
+  const checks = {
+    n: false,
+    ne: false,
+    e: false,
+    se: false,
+    s: false,
+    sw: false,
+    w: false,
+    nw: false,
+  };
   for (let i = 0; i < n; i++) {
     board.push([]);
     for (let j = 0; j < n; j++) {
@@ -10,25 +21,105 @@ function queensAttack(n, k, r_q, c_q, obstacles) {
 
   board[r_q][c_q] = "Q";
 
-  obstacles.forEach((obstacle) => {
-    board[obstacle[0]][obstacle[1]] = "X";
-  });
+  if (obstacles) {
+    obstacles.forEach((obstacle) => {
+      board[obstacle[0]][obstacle[1]] = "X";
+    });
+  }
 
-  const distances = {
-    n: 0,
-    ne: 0,
-    e: 0,
-    se: 0,
-    s: 0,
-    sw: 0,
-    w: 0,
-    nw: 0,
-  };
+  let left = c_q - 1;
+  let straight = c_q;
+  let right = c_q + 1;
+  //north
 
-  console.log(board);
+  for (let i = r_q - 1; i >= 0; i--) {
+    if (checks.nw === true) {
+    } else if (board[i][left] !== "X" && left >= 0) {
+      board[i][left] = "o";
+      counter++;
+    } else if (board[i][left] === "X") {
+      checks.nw = true;
+    }
+    if (checks.n === true) {
+    } else if (board[i][straight] !== "X") {
+      board[i][straight] = "o";
+      counter++;
+    } else if (board[i][straight] === "X") {
+      checks.n = true;
+    }
+    if (checks.ne === true) {
+    } else if (board[i][right] !== "X" && right < n) {
+      board[i][right] = "o";
+      counter++;
+    } else if (board[i][right] === "X") {
+      checks.ne = true;
+    }
+    left -= 1;
+    right += 1;
+  }
+
+  left = c_q - 1;
+  straight = c_q;
+  right = c_q + 1;
+
+  for (let i = r_q + 1; i < n; i++) {
+    if (checks.sw === true) {
+    } else if (board[i][left] !== "X" && left >= 0) {
+      board[i][left] = "o";
+      counter++;
+    } else if (board[i][left] === "X") {
+      checks.sw = true;
+    }
+    if (checks.s === true) {
+    } else if (board[i][straight] !== "X") {
+      board[i][straight] = "o";
+      counter++;
+    } else if (board[i][straight] === "X") {
+      checks.s = true;
+    }
+    if (checks.se === true) {
+    } else if (board[i][right] !== "X" && right < n) {
+      board[i][right] = "o";
+      counter++;
+    } else if (board[i][right] === "X") {
+      checks.se = true;
+    }
+    left -= 1;
+    right += 1;
+  }
+
+  left = c_q - 1;
+  right = c_q + 1;
+
+  for (let i = c_q - 1; i >= 0; i--) {
+    if (checks.w === true) {
+    } else if (board[r_q][i] !== "X") {
+      board[r_q][i] = "o";
+      counter++;
+    } else if (board[r_q][i] === "X") {
+      checks.w = true;
+    }
+  }
+
+  for (let i = c_q + 1; i < n; i++) {
+    if (checks.e === true) {
+    } else if (board[r_q][i] !== "X") {
+      board[r_q][i] = "o";
+      counter++;
+    } else if (board[r_q][i] === "X") {
+      checks.e = true;
+    }
+  }
+  return counter;
+  console.log(counter);
 }
 
-queensAttack(5, 2, 1, 1, [
-  [2, 2],
-  [3, 1],
+// queensAttack(5, 2, 3, 5, [
+//   [2, 2],
+//   [3, 1],
+// ]);
+
+queensAttack(6, 2, 2, 2, [
+  [3, 2],
+  [1, 3],
 ]);
